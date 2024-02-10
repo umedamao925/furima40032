92ebf1ce8883adef8a15a3cbceac8f87f01607c6
umedamao925 <umedamao.0925@gmail.com>
2024-02-10 21:57:24 +0900

    商品出品機能単体テスト

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,18 +3,16 @@ class Item < ApplicationRecord
   has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
-    #空の投稿を保存できないようにする
 
-  #ジャンルの選択が「---」の時は保存できないようにする
- validates :category_id, numericality: { other_than: 1 } 
+
 
      #ジャンルの選択が「---」の時は保存できないようにする
-  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :category_id, :condition_id, :cost_bearer_id, :shipping_area_id, :delivery_day_id, numericality: { other_than: 1 , message: " を入力してください"}
 
   validates :price, inclusion: { in: 300..9_999_999 }, format: { with: /\A[0-9]+\z/ }
   
-  validates :item_name, :explain, :category_id, :condition_id, :cost_bearer_id, :shipping_area_id, :delivery_day_id, :price, presence: true
+  validates :item_name, :explain, :price, :image, presence: true
 
-  validates :price, numericality: true
+
 
 end
