6e8e8478e60fa2730beb69739b97f697df6a3cbd
umedamao925 <umedamao.0925@gmail.com>
2024-02-12 15:10:50 +0900

    FB修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
 
-  def move_to_index
-    @item = Item.find(params[:id])
-    unless user_signed_in? && current_user.id == @item.user_id
-      redirect_to action: :index
-    end
-  end
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -29,10 +23,20 @@ class ItemsController < ApplicationController
   def show
   end
 
+  
+
   private
  
   def item_params
     params.require(:item).permit(:item_name, :explain, :category_id, :condition_id, :cost_bearer_id, :shipping_area_id, :delivery_day_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to action: :index
+    end
   end
 
 end
