b560edbd7392bc41af6d7404e21846d836ed7b2c
umedamao925 <umedamao.0925@gmail.com>
2024-02-14 21:49:02 +0900

    FB修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :destroy]
   before_action :set_item, only: [:edit, :show, :update, :destroy]
+  before_action :login_item, only: [:edit, :destroy]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -31,9 +32,6 @@ end
   def destroy
     @item.destroy
     redirect_to root_path
-    unless current_user.id == @item.user_id
-      redirect_to root_path
-    end
   end
 
   def update
@@ -55,6 +53,13 @@ end
   def set_item
     @item = Item.find(params[:id])
    end
+
+   private
+   def login_item
+    unless current_user.id == @item.user_id
+      redirect_to root_path
+    end
+  end
   
 
 end
