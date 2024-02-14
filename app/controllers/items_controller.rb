34f88b669a90dc0bd610ed86e1c842a6cdfc3c2a
umedamao925 <umedamao.0925@gmail.com>
2024-02-14 20:41:31 +0900

    FB修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :destroy]
-
+  before_action :set_item, only: [:edit, :show, :update]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -11,7 +11,6 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @item = Item.find(params[:id])
     unless current_user.id == @item.user_id
       redirect_to root_path
   end
@@ -27,13 +26,11 @@ end
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
-    redirect_to root_path
+    redirect_to item_path(@item.id)
   else
     render :edit, status: :unprocessable_entity 
   end
@@ -42,10 +39,14 @@ end
   
 
   private
- 
   def item_params
     params.require(:item).permit(:item_name, :explain, :category_id, :condition_id, :cost_bearer_id, :shipping_area_id, :delivery_day_id, :price, :image).merge(user_id: current_user.id)
   end
+
+  private
+  def set_item
+    @item = Item.find(params[:id])
+   end
   
 
 end
