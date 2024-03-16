f48c3a4848cb7f3f1672fc3c035883a33d2d8b5c
umedamao925 <umedamao.0925@gmail.com>
2024-03-16 17:57:56 +0900

    修正3

--- app/models/delivery_day.rb
+++ app/models/delivery_day.rb
@@ -3,7 +3,7 @@ class DeliveryDay < ActiveHash::Base
     { id: 1, name: '---' },
     { id: 2, name: '1日〜2日で発送' },
     { id: 3, name: '2日〜3日で発送' },
-    { id: 4, name: '3日〜7日で発送' },
+    { id: 4, name: '4日〜7日で発送' },
 
   ]
  
