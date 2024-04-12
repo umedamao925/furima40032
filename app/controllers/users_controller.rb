b4bc731c86b82189e891dc3570214cce7a16d65a
umedamao925 <umedamao.0925@gmail.com>
2024-04-12 15:15:44 +0900

    修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,9 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:likes]
 
   def likes
-    @item = Item.find(params[:item_id])
     likes = Like.where(user_id: @user.id).pluck(:item_id)
     @like_items = current_user.likes.map(&:item)
-    @like_counts = @like_items.map { |item_id| Like.where(item_id: item_id).count }.sum
+    @like_counts = Like.where(item_id: @like_items).count
   end
   
   private
